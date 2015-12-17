6a624a4a69211571fb6864f6a55310fcb98602a0
Digitality 5 <Digitality 5@Zbox>
2015-12-17 14:28:06 -0600

    Redirección a lista de problemas y mostrarlos aleatoriamente

--- src/SonodigestBundle/Controller/ProblemadetalleController.php
+++ src/SonodigestBundle/Controller/ProblemadetalleController.php
@@ -18,12 +18,53 @@ class ProblemadetalleController extends Controller{
     /**
      * Lists all Categoria entities.
      *
-     * @Route("/", name="admin_problemadetalle")
+     * @Route("/{id}", name="admin_problemadetalle")
      * @Method("GET")
-     * @Template()
+     * @Template("SonodigestBundle:Problemadetalle:problemadetalle.html.twig")
      */
-    public function indexAction()
+    public function indexAction($id)
     {
-         return $this->render('SonodigestBundle:Problemadetalle:problemadetalle.html.twig');     
+        
+        $em = $this->getDoctrine()->getManager();
+
+//      $entities = $em->getRepository('SonodigestBundle:Categoriablog')->findAll();
+        $entities = $em->getRepository('SonodigestBundle:Problema')->find($id);
+        $idMax = $em->getRepository('SonodigestBundle:Problema')->findBy(array(),array('id'=>'DESC'));
+        //var_dump($idMax);
+        //$entities = $em->getRepository('SonodigestBundle:Problema')->findAll();
+        $idMax = $idMax[0]->getId();
+        $encontrado=true;
+        $problemas_random=array();
+        $idRecuperados=array();
+        //var_dump($problemas_random);
+        while(count($problemas_random)<4){
+            
+            
+            $random = rand(1,$idMax);
+            //var_dump($random);
+            if($random!=$id){
+                $prob= $em->getRepository('SonodigestBundle:Problema')->find($random);
+                
+                if(!in_array($random, $idRecuperados, true)){
+                    
+                
+                    if(count($prob)!=0){
+                        array_push ($problemas_random, $prob);
+                        array_push ($idRecuperados, $random);
+                    }
+                }
+                    
+                
+            }
+        }
+        //var_dump($problemas_random);
+        //var_dump($idRecuperados);
+        
+        
+        
+        return array(
+            'problema'=>$entities,
+            'problemasRandom'=>$problemas_random,
+        );     
     }
 }
